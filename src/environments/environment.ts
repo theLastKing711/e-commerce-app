772a35ecd2aae321d5aea3f921b39292c12721c2
theLastKing711 <lastking711@protonmail.com>
2022-09-27 03:15:07 +0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  // base_url: "https://e-commerce-api1.herokuapp.com/api/",
-  // imagesPath: "https://e-commerce-api1.herokuapp.com/api/images/"
-  base_url: "https://localhost:7267/api/",
-  imagesPath: "https://localhost:7267/images/"
+  base_url: "https://e-commerce-api1.herokuapp.com/api/",
+  imagesPath: "https://e-commerce-api1.herokuapp.com/api/images/"
+  // base_url: "https://localhost:7267/api/",
+  // imagesPath: "https://localhost:7267/images/"
 };
 
 /*
